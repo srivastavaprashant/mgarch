04174323210ec1f8732581dd464479ea0df4de4c
Prashant Srivastava <srivastava.prashant898@gmail.com>
2020-07-21 05:08:13 +0100

    configuration update

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # mgarch
  DCC-GARCH(1,1) for multivariate normal distribution. 
+Use case:
+# rt = (t, n) numpy matrix with t days of observation and n number of assets.
+import mgarch
+vol = mgarch(rt)
+vol.fit()
+cov_nextday = vol.predict()
---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'mgarch',         
+  packages = ['mgarch'],   
+  version = '0.1',      
+  license='afl-3.0',    
+  description = 'DCC-GARCH(1,1)',
+  author = 'Prashant Srivastava',
+  author_email = 'srivastava.prashant898@gmail.com',
+  url = 'https://github.com/srivastavaprashant/mgarch',
+  download_url = 'https://github.com/srivastavaprashant/mgarch/archive/0.1.tar.gz',
+  keywords = ['volatility', 'multivariate', 'garch'],
+  install_requires=[
+          'numpy',
+          'scipy',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: Academic Free License v3.0',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
